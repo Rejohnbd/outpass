e75d3a9bd6f913982aa963a855d455afcef98516
unknown <rejohn.cse@gmail.com>
2024-01-20 22:18:25 +0600

    Change Login Blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,107 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.guest')
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+@section('title', 'Login')
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+@section('content')
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+<div class="row signpages text-center">
+    <div class="col-md-12">
+        <div class="card border-0">
+            <div class="row row-sm">
+                <div class="col-lg-6 col-xl-6 col-xs-12 col-sm-12 login_form rounded-start-11">
+                    <div class="container-fluid">
+                        <div class="row row-sm">
+                            <div class="card-body mt-2 mb-2">
+                                <div class="mobilelogo">
+                                    <img src="{{ asset('assets/img/brand/logo.png') }}" class=" d-lg-none header-brand-img text-start float-start mb-4 dark-logo" alt="logo">
+                                    <img src="{{ asset('assets/img/brand/logo-light.png') }}" class=" d-lg-none header-brand-img text-start float-start mb-4 light-logo" alt="logo">
+                                </div>
+                                <div class="clearfix"></div>
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <h2 class=" text-start mb-2">Sign In</h2>
+                                    <p class="mb-4 text-muted tx-13 ms-0 text-start">Sign in to Create, Discover and Connect with the Global Community</p>
+                                    <div class="panel desc-tabs border-0 p-0">
+                                        <div class="panel-body tabs-menu-body mt-2">
+                                            <div class="tab-pane active" id="tab01">
+                                                <div class="form-group text-start">
+                                                    <label class="tx-medium">Email</label>
+                                                    <input class="form-control" placeholder="Enter your email" type="email" name="email" autocomplete="email">
+                                                    @error('email')
+                                                    <div class="invalid-feedback" style="display: block;">{{ $message }}</div>
+                                                    @enderror
+                                                </div>
+                                                <div class="form-group text-start">
+                                                    <label class="tx-medium">Password</label>
+                                                    <input class="form-control border-end-0" placeholder="Enter your password" type="password" name="password" autocomplete="new-password" data-bs-toggle="password">
+                                                    @error('password')
+                                                    <div class="invalid-feedback" style="display: block;">{{ $message }}</div>
+                                                    @enderror
+                                                </div>
+                                                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                                <div class="text-start mt-4 ms-0 mb-3">
+                                    <div class="mb-1"><a href="forgot.html">Forgot password?</a></div>
+                                    <div>Don't have an account? <a href="signup.html">Register Here</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xl-6 d-none d-lg-block text-center bg-primary details rounded-end-11">
+                    <div class="mt-4 pt-4 p-2 pos-relative">
+                        <img src="{{ asset('assets/img/brand/logo-light.png') }}" class="header-brand-img mb-3 mt-3" alt="logo">
+                        <div class="clearfix"></div>
+                        <img src="{{ asset('assets/img/pngs/user.png') }}" class="ht-250 mb-0" alt="user">
+                        <h2 class="mt-4 text-white tx-normal">Sign In Your Account</h2>
+                        <span class="tx-white-6 tx-13 mb-5 mt-xl-0">Sign in to Create, Discover and Connect with the Global Community</span>
+                    </div>
+                </div>
             </div>
+        </div>
+    </div>
+</div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+{{-- <form method="POST" action="{{ route('login') }}">
+@csrf
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+<!-- Email Address -->
+<div>
+    <x-label for="email" :value="__('Email')" />
 
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+</div>
+
+<!-- Password -->
+<div class="mt-4">
+    <x-label for="password" :value="__('Password')" />
+
+    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+</div>
+
+<!-- Remember Me -->
+<div class="block mt-4">
+    <label for="remember_me" class="inline-flex items-center">
+        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+    </label>
+</div>
+
+<div class="flex items-center justify-end mt-4">
+    @if (Route::has('password.request'))
+    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+        {{ __('Forgot your password?') }}
+    </a>
+    @endif
+
+    <x-button class="ml-3">
+        {{ __('Log in') }}
+    </x-button>
+</div>
+</form> --}}
+@endsection
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,24 +1,39 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{ asset('assets/img/brand/favicon.ico') }}" type="image/x-icon" />
+    <title>{{ config('app.name', 'Laravel') }} :: @yield('title')</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link id="style" rel="stylesheet" href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/web-fonts/icons.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/web-fonts/font-awesome/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/web-fonts/plugin.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }}">
+</head>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+<body>
+    <div class="main-body leftmenu ltr light-theme">
+        <div id="global-loader">
+            <img src="{{ asset('assets/img/loader.svg') }}" class="loader-img" alt="Loader">
         </div>
-    </body>
+
+        <div class="page main-signin-wrapper">
+            @yield('content')
+        </div>
+    </div>
+
+    <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/bootstrap/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap-show-password.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('assets/js/themeColors.js') }}"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
+</body>
+
 </html>
